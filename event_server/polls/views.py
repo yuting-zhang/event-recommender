53291c90a375b407dfa3d25f9394e494980174c8
Shunping Xie <sxie12@illinois.edu>
2018-05-02 17:12:04 -0500

    done with tutorial

--- event_server/polls/views.py
+++ event_server/polls/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Question
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        x = Question()
+        x.id = 34
+        x.question_text = 'Does this work?'
+        y = Question()
+        y.id = 99
+        y.question_text = 'Yes it does'
+        rando_list = [x, y]
+        return rando_list
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""
+def index(response):
     template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': 'Does this work?',
-    }
+    x = Question()
+    x.id = 34
+    x.question_text = 'Does this work?'
+    y = Question()
+    y.id = 99
+    y.question_text = 'Yes it does'
+    rando_list = [x, y]
+    context = { 'latest_question_list': rando_list }
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
@@ -16,6 +50,7 @@ def detail(request, question_id):
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+    """
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
