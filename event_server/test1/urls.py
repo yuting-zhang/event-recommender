cbd5834e6751b5c9e9170fac676e60a889cd4480
Shunping Xie <sxie12@illinois.edu>
2018-05-06 19:29:17 -0500

    reverted to old django format

--- event_server/test1/urls.py
+++ event_server/test1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'test1'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
 ]
--- event_server/test1/views.py
+++ event_server/test1/views.py
@@ -12,12 +12,7 @@ from django.core.files.base import ContentFile
 from .models import TestClass
 from .forms import CheckboxForm
 
-# Create your views here.
-class IndexView(generic.ListView):
-    template_name = 'test1/index.html'
-    context_object_name = 'test_context'
-
-    def get_queryset(self):
-    	x = TestClass()
-    	x.set_values(1, 12.345, 'testing', False, '05 03 2018')
-    	return [x]
+def index(request):
+    x = TestClass()
+    x.set_values(1, 12.345, 'testing', False, '05 03 2018')
+    return render(request, 'test1/index.html', context={'testing':x},)
