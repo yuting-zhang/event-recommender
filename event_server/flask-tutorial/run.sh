45b18730ee520a4c50b46b0002638ad93b0bb3bd
Shunping Xie <sxie12@illinois.edu>
2018-05-10 10:54:03 -0500

    deleted old files

--- event_server/flask-tutorial/run.sh
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=flaskr
-export FLASK_ENV=development
-python3 -m flask run --host=0.0.0.0 --port=80
--- event_server/test1/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class CheckboxForm(forms.Form):
-    ai = forms.BooleanField(required=False, initial=False, label='Artificial Intelligence')
