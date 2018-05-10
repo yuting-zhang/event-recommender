5148dcd10532c2a83ab2feec4414abe2eb389427
Shunping Xie <sxie12@illinois.edu>
2018-05-10 10:25:12 -0500

    updated recommender

--- event_server/flask-tutorial/flaskr/recommender/recommender.py
+++ event_server/flask-tutorial/flaskr/recommender/recommender.py
@@ -174,15 +174,15 @@ class Recommender:
         for word in self.user_data[username]:
             self.user_data[username][word] *= self.alpha
 
-        if len(lst_rel) > 0:
-            w = self.beta / len(lst_rel)
+        if len(lst_non_rel) > 0:
+            w = self.gamma / len(lst_non_rel)
             for idx in lst_non_rel:
                 doc_dict = self.get_word_count(self.test_docs_stemmed[idx])
                 for word in doc_dict:
                     self.user_data[username][word] -= w * doc_dict[word]
 
-        if len(lst_non_rel) > 0:
-            w = self.gamma / len(lst_non_rel)
+        if len(lst_rel) > 0:
+            w = self.beta / len(lst_rel)
             for idx in lst_rel:
                 doc_dict = self.get_word_count(self.test_docs_stemmed[idx])
                 for word in doc_dict:
